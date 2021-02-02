f2132534274411631421960e3e734a8b3d530276
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-02-01 21:24:06 -0500

    crude workaround, print call stack string

--- minimal_log.py
+++ minimal_log.py
@@ -1,8 +1,8 @@
+from inspect import FrameInfo
 import inspect
 import logging
 import os
 import pathlib2
-import sys
 
 ERROR_LEVELS = (logging.INFO, logging.DEBUG,
                 logging.WARNING, logging.ERROR)
@@ -20,8 +20,8 @@ class MinimalLog:
             self.configure(overwrite=False)
             if debug:
                 self.__debug()
-        except RuntimeError as r_err:
-            self.log_exception(r_err)
+        except Exception as e_err:
+            self.log_exception(e_err, level=self.ERROR)
 
     def clean_up(self):
         """
@@ -31,8 +31,8 @@ class MinimalLog:
         try:
             log_files = self.find_all_files_with_extension('.log')
             self.delete_list_of_files(log_files)
-        except OSError as os_err:
-            self.log_exception(os_err)
+        except Exception as es_err:
+            self.log_exception(es_err, level=self.ERROR)
 
     def configure(self, overwrite=True):
         """
@@ -47,19 +47,19 @@ class MinimalLog:
                                 filemode=filemode,
                                 level=self.get_default_level(),
                                 format=self.log_format)
-        except RuntimeError as r_err:
-            self.log_exception(r_err)
+        except Exception as e_err:
+            self.log_exception(e_err, level=self.ERROR)
 
     def delete_list_of_files(self, files_to_delete: list):
         """
         :param files_to_delete: self-documenting
         :return: None
         """
-        for file in files_to_delete:
-            try:
+        try:
+            for file in files_to_delete:
                 os.remove(file)
-            except OSError as o_err:
-                self.log_exception(o_err)
+        except Exception as e_err:
+            self.log_exception(e_err, level=self.ERROR)
 
     def find_all_files_with_extension(self, extension='.log') -> list:
         """
@@ -73,8 +73,8 @@ class MinimalLog:
                     if file.endswith('.log'):
                         list_of_log_files.append(pathlib2.Path(root, file).absolute())
             return list_of_log_files
-        except OSError as o_err:
-            self.log_exception(o_err)
+        except Exception as e_err:
+            self.log_exception(e_err, level=self.ERROR)
 
     def get_default_level(self) -> int:
         """
@@ -82,8 +82,8 @@ class MinimalLog:
         """
         try:
             return logging.INFO
-        except ValueError as v_err:
-            self.log_exception(v_err)
+        except Exception as e_err:
+            self.log_exception(e_err, level=self.ERROR)
 
     def get_format_strings(self):
         """
@@ -138,11 +138,14 @@ class MinimalLog:
         """
         try:
             self.logger.exception(msg=str(exception), level=level)
-        except OSError as o_err:
-            print(o_err)
+        except Exception as e_err:
+            print(e_err)
 
-    def log_event(self, event, event_completed=None, level=logging.INFO,
-                  announce=False):
+    def log_event(self, event,
+                  event_completed=None,
+                  level=logging.INFO,
+                  announce=False,
+                  log_call_stack=False):
         # TODO bug.. no matter where this function is located it always says it's own name for %(funcName)s in log..
         # TODO ..file. should fix that, would make debugging easier if it called the name of the function calling it
         """
@@ -150,47 +153,42 @@ class MinimalLog:
         :param event_completed: whether this is the beginning or end of the event
         :param level: manual override of the default logging level
         :param announce: bool, create a visible distinction in the log file
+        :param log_call_stack: bool, set to True for more spam about function calls
         :return: None
         """
-        function_name_index = 3
-        stack = _get_stack()
-        stack_depth = _get_stack_depth(stack)
-        function_stack = list()
-        for stack_index in range(stack_depth):
-            function_stack.append(_get_stack()[stack_index][function_name_index])
-        f_name = stack[0][function_name_index]
-        # TODO still don't have calling function.. see BUG details above..
-        if not self.string_is_valid(event):
+        if not _string_is_valid(event):
             try:
                 event = str(event)
-            except TypeError as t_err:
-                self.log_exception(t_err)
-        if event_completed is True:
-            event = 'success : ' + event
-        elif event_completed is False:
-            event = 'attempt : ' + event
-        try:
-            if announce:
-                self.logger.log(level=level, msg=ANNOUNCE(event))
-                print(ANNOUNCE(event))
-                return
-            self.logger.log(level=level, msg=event)
-            print(event)
-            return
-        except RuntimeError as r_err:
-            print(r_err)
+            except Exception as e_err:
+                print(e_err)
 
-    def string_is_valid(self, string_to_check):
-        """
-        :param string_to_check: self documenting
-        :return: bool, valid string or not
-        """
+        events_to_log = [event]
+        if log_call_stack:
+            function_names_in_call_stack = _get_function_names_in_call_stack()
+            stack_call_string = ''
+            for function_name in function_names_in_call_stack:
+                if stack_call_string == '':
+                    stack_call_string = function_name
+                    continue
+                stack_call_string += ' < ' + function_name
+            events_to_log.append(stack_call_string)
+
+        for index, event in enumerate(events_to_log):
+            if event_completed is True:
+                event = 'success : ' + event
+            elif event_completed is False:
+                event = 'attempt : ' + event
+            events_to_log[index] = event
+
         try:
-            if isinstance(string_to_check, str):
-                return True
-            return False
-        except TypeError as t_err:
-            self.log_exception(t_err)
+            for event in events_to_log:
+                if announce:
+                    self.logger.log(level=level, msg=ANNOUNCE(event))
+                    print(ANNOUNCE(event))
+                self.logger.log(level=level, msg=event)
+                print(event)
+        except Exception as e_err:
+            print(e_err)
 
     def __debug(self):
         """
@@ -199,13 +197,38 @@ class MinimalLog:
         """
         self.log_event(event='meaningless debug event', event_completed=False, announce=True)
         for i in range(9):
-            self.log_event(event='intermediate event number {}'.format(i))
+            self.log_event(event='intermediate event number {}'.format(i), log_call_stack=True)
         self.log_event(event='meaningless debug event', event_completed=True, announce=True)
 
 
-def _get_stack() -> inspect.stack:
+def _get_caller_name_from_(frame: FrameInfo) -> str:
+    try:
+        caller_name = ''
+        frame_at_function_name_index = frame[3]
+        if frame_at_function_name_index is not None:
+            caller_name = frame_at_function_name_index
+        return caller_name
+    except Exception as e_err:
+        print(e_err)
+
+
+def _get_function_names_in_call_stack():
+    try:
+        stack = _get_stack(filter_system_frames=True)
+        function_names_in_call_stack = list()
+        for frame in stack:
+            caller = _get_caller_name_from_(frame)
+            function_names_in_call_stack.append(caller)
+        return function_names_in_call_stack
+    except Exception as e_err:
+        print(e_err)
+
+
+def _get_stack(filter_system_frames=True) -> inspect.stack:
     try:
         stack = inspect.stack()
+        if filter_system_frames:
+            stack = _remove_system_frames_from_(stack)
         return stack
     except Exception as e_err:
         print(e_err)
@@ -213,7 +236,7 @@ def _get_stack() -> inspect.stack:
 
 def _get_stack_depth(stack) -> int:
     try:
-        assert stack, 'assertion failed, stack has no value'
+        assert stack, 'assertion failed, stack has Falsy value'
         stack_depth_count = 0
         for _ in stack:
             stack_depth_count += 1
@@ -222,8 +245,62 @@ def _get_stack_depth(stack) -> int:
         print(e_err)
 
 
+def _is_non_system_(caller_name: str) -> bool:
+    """
+    valid in the current context means, "non-system" calls
+    :param caller_name: a caller name, can be a function, module, dunder, etc
+    :return: bool, True = valid, False = invalid
+    """
+    system_calls = [
+        '_call_with_frames_removed',
+        '_exec',
+        'exec_module',
+        'execfile',
+        '_find_and_load',
+        '_find_and_load_unlocked',
+        '_load_unlocked'
+                    ]
+    try:
+        if caller_name is None:  # filter None
+            return False
+        if caller_name in system_calls:
+            return False
+        if caller_name.endswith('__') or caller_name.endswith('>'):  # filter dunder, <module>,
+            return False
+        return True
+        pass
+    except Exception as e_err:
+        print(e_err)
+
+
+def _remove_system_frames_from_(stack: inspect.stack) -> list:
+    try:
+        non_system_frames = list()
+        for frame in stack:
+            caller_name = _get_caller_name_from_(frame)
+            if _is_non_system_(caller_name):
+                non_system_frames.append(frame)
+        return non_system_frames
+    except Exception as e_err:
+        print(e_err)
+
+
+def _string_is_valid(string_to_check):
+    """
+    :param string_to_check: self documenting
+    :return: bool, valid string or not
+    """
+    try:
+        if isinstance(string_to_check, str):
+            return True
+        return False
+    except Exception as e_err:
+        print(e_err)
+
+
 if __name__ == '__main__':
     from data_src.CONSTANTS import *
     sl = MinimalLog(debug=True)
+    pass
 else:
     from .data_src.CONSTANTS import *
